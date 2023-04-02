a52db3d72e370a5f5af94d9701537740d5436336
Jan G <apollg3@gmail.com>
2023-04-02 10:35:20 +0800

    useDebounceCallback

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,14 +1,27 @@
 import { useRef } from "react";
-import Input from "~/modules/common/atoms/Input";
-import Container from "~/modules/common/molecules/Container";
+import Input from "~/modules/common/components/atoms/Input";
+import Container from "~/modules/common/components/molecules/Container";
+import useDebounceCallback from "~/modules/common/hooks/useDebounceCallback";
 
 const SearchPage = () => {
   const superHeroNameRef = useRef<HTMLInputElement>(null);
 
+  const onChange = () => {
+    console.log(superHeroNameRef.current?.value);
+  };
+
+  const debouncedOnchange = useDebounceCallback(onChange);
+
   return (
     <section className="bg-gray-50">
       <Container>
-        <Input placeholder="Search for a superhero" ref={superHeroNameRef} />
+        <Input
+          placeholder="Search for a superhero"
+          ref={superHeroNameRef}
+          onChange={async () => {
+            await debouncedOnchange();
+          }}
+        />
       </Container>
     </section>
   );
